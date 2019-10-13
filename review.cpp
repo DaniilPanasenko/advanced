3abc19137cb2ab398bfc50f81c0c65404e1bf6e9
DaniilPanasenko <56221634+DaniilPanasenko@users.noreply.github.com>
2019-10-13 10:23:21 +0300

    Add files via upload

---
+++ review.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+#include <limits>
+#include <algorithm>
+
+std::vector<int> array_input(int num) {   }
+
+void output(std::vector<int> answer) {   }
+
+std::vector<int> task_B(int num, std::vector<int> arr) {   }
+
+int main() {
+	int num;
+	std::cin >> num;
+	std::vector<int> arr = array_input(num);
+	std::vector<int> answer = task_B(num, arr);
+	output(answer);
+	return 0;
+}
