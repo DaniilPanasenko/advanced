26d9d70effc3620f6efcf45e831a390f07ce3111
DaniilPanasenko <56221634+DaniilPanasenko@users.noreply.github.com>
2019-10-13 19:36:02 +0300

    Add files via upload

--- review.cpp
+++ review.cpp
@@ -3,15 +3,15 @@
 #include <limits>
 #include <algorithm>
 
-std::vector<int> input();
+std::vector<int> input(FILE* stdin);
 
-void output(const std::vector<int> answer);
+void output(FILE* stdout, const std::vector<int>& answer);
 
-std::vector<int> task_B(const std::vector<int> input_array);
+std::vector<int> task_B(const std::vector<int>& array_coefficients_demand);
 
 int main() {
-	const std::vector<int> input_array = input();
-	const std::vector<int> answer = task_B(input_array);
-	output(answer);
+	const std::vector<int> array_coefficients_demand = input(stdin);
+	const std::vector<int> answer = task_B(array_coefficients_demand);
+	output(stdout, answer);
 	return 0;
 }
