7a975007ef77f38c8ae1505aa66566bcefd6a635
DaniilPanasenko <56221634+DaniilPanasenko@users.noreply.github.com>
2019-10-13 16:15:32 +0300

    Add files via upload

--- review.cpp
+++ review.cpp
@@ -7,11 +7,11 @@ std::vector<int> input();
 
 void output(const std::vector<int> answer);
 
-std::vector<int> task_B(const int input_array_size, const std::vector<int> input_array);
+std::vector<int> task_B(const std::vector<int> input_array);
 
 int main() {
 	const std::vector<int> input_array = input();
-	const std::vector<int> answer = task_B(input_array.size(), input_array);
+	const std::vector<int> answer = task_B(input_array);
 	output(answer);
 	return 0;
 }
