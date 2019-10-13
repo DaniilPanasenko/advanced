c3ae1b27de06bbcc837cd1df86cd5eb78b001edb
DaniilPanasenko <56221634+DaniilPanasenko@users.noreply.github.com>
2019-10-13 20:57:06 +0300

    Add files via upload

--- review.cpp
+++ review.cpp
@@ -3,15 +3,15 @@
 #include <limits>
 #include <algorithm>
 
-std::vector<int> input(std::istream& input = std::cin);
+std::vector<int> reading_demand_coefficients(std::istream& input = std::cin);
 
-void output(const std::vector<int>& numbers_of_workers_in_video, std::ostream& output = std::cout);
+void writing_workers_indices(const std::vector<int>& workers_indices, std::ostream& output = std::cout);
 
-std::vector<int> main_algorithm(const std::vector<int>& demand_coefficients);
+std::vector<int> searching_video_partipants(const std::vector<int>& demand_coefficients);
 
 int main() {
-    const std::vector<int> demand_coefficients = input();
-    const std::vector<int> worker_numbers_in_video = main_algorithm(demand_coefficients);
-    output(worker_numbers_in_video);
+    const std::vector<int> demand_coefficients = reading_demand_coefficients();
+    const std::vector<int> workers_indices = searching_video_partipants(demand_coefficients);
+    writing_workers_indices(workers_indices);
     return 0;
 }
