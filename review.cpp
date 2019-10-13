99251e3f598408c7eabb2a2d7ecb245e70c75991
DaniilPanasenko <56221634+DaniilPanasenko@users.noreply.github.com>
2019-10-13 21:18:12 +0300

    Add files via upload

--- review.cpp
+++ review.cpp
@@ -10,7 +10,7 @@ void write_workers_indices(const std::vector<int>& workers_indices, std::ostream
 std::vector<int> find_maximal_subsequence(const std::vector<int>& demand_coefficients);
 
 int main() {
-    const std::vector<int> demand_coefficients = reade_demand_coefficients();
+    const std::vector<int> demand_coefficients = read_demand_coefficients();
     const std::vector<int> workers_indices = find_maximal_subsequence(demand_coefficients);
     write_workers_indices(workers_indices);
     return 0;
