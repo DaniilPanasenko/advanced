3666edcf5251307d25017f9cab7cd24773076b0a
DaniilPanasenko <56221634+DaniilPanasenko@users.noreply.github.com>
2019-10-13 21:15:39 +0300

    Add files via upload

--- review.cpp
+++ review.cpp
@@ -3,15 +3,15 @@
 #include <limits>
 #include <algorithm>
 
-std::vector<int> reading_demand_coefficients(std::istream& input = std::cin);
+std::vector<int> read_demand_coefficients(std::istream& input = std::cin);
 
-void writing_workers_indices(const std::vector<int>& workers_indices, std::ostream& output = std::cout);
+void write_workers_indices(const std::vector<int>& workers_indices, std::ostream& output = std::cout);
 
-std::vector<int> searching_video_partipants(const std::vector<int>& demand_coefficients);
+std::vector<int> find_maximal_subsequence(const std::vector<int>& demand_coefficients);
 
 int main() {
-    const std::vector<int> demand_coefficients = reading_demand_coefficients();
-    const std::vector<int> workers_indices = searching_video_partipants(demand_coefficients);
-    writing_workers_indices(workers_indices);
+    const std::vector<int> demand_coefficients = reade_demand_coefficients();
+    const std::vector<int> workers_indices = find_maximal_subsequence(demand_coefficients);
+    write_workers_indices(workers_indices);
     return 0;
 }
