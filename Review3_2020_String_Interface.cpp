12e19b31db50a03626bb0e17fff0cafc59f9640d
DaniilPanasenko <56221634+DaniilPanasenko@users.noreply.github.com>
2020-06-14 20:26:33 +0300

    Update Review3_2020_String_Interface.cpp

--- Review3_2020_String_Interface.cpp
+++ Review3_2020_String_Interface.cpp
@@ -2,27 +2,37 @@
 #include <vector>
 #include <cctype>
 
-struct Node{
-    std::vector<Node*> childs;
-    bool finally;
-    int count;
-    char letter;
-    explicit Node(char character);
+class Lottery{
+ private:
+    class Node{
+     public:
+        std::vector<Node*> childs;
+        bool finally;
+        int count;
+        char letter;
+
+        explicit Node(char character);
+
+        int search_letter(char searched);
+
+        int add_letter(char added);
+    };
+
+    Node* list_of_people;
+
+ public:
+    Lottery();
+
+    void add_people(std::string name);
+
+    std::string search_winner(int number_winner);
 };
 
-int search_letter(Node* node, char searched);
-
-int add_letter(Node* node, char added);
-
-void add_people(Node* list_of_people, std::string name);
-
-std::string search_winner(Node* list_of_people, int number_winner);
-
-std::vector<std::string> get_commands();
+std::vector<std::string> get_commands(std::istream& input = std::cin);
 
 std::vector<std::string> get_winners(std::vector<std::string> commands);
 
-void print_winners(std::vector<std::string> winners);
+void print_winners(std::vector<std::string> winners, std::ostream& output = std::cout);
 
 int main()
 {
